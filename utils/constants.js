ab37fa144b6f5e31c45ebbe22ebac84ef4343284
CollinsTYK <mattycollins803@gmail.com>
2026-03-20 20:15:39 +0100

    Create constants file

---
+++ utils/constants.js
@@ -0,0 +1,15 @@
+exports.PurchaseState = {
+  DRAFT: "DRAFT",
+  CONFIRMED: "CONFIRMED",
+  CANCELLED: "CANCELLED",
+};
+exports.ReceivingStatus = {
+  NOT_RECEIVED: "NOT_RECEIVED",
+  PARTLY_RECEIVED: "PARTLY_RECEIVED",
+  RECEIVED: "RECEIVED",
+};
+exports.PaymentStatus = {
+  NOT_PAID: "NOT_PAID",
+  PARTLY_PAID: "PARTLY_PAID",
+  PAID: "PAID",
+};

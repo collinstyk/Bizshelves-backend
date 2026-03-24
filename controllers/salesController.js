60d3b37fb79c8192bd0d94ed6318920ed0185d9c
CollinsTYK <mattycollins803@gmail.com>
2026-03-24 12:12:34 +0100

    Update recordSale function

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,18 +1,76 @@
-const CompanyProduct = require("../models/companyProductModel");
-const SaleModel = require("../models/saleModel");
+const mongoose = require("mongoose");
+
 const catchAsync = require("../utils/catchAsync");
 
+const {
+  resolveCustomerId,
+  createSale,
+  validateItems,
+  getCompanyProductMap,
+  validateStock,
+  createSaleItems,
+  deductStock,
+  calculateTotalSaleAmount,
+  getPaymentStatus,
+  updateSale,
+} = require("../services/sale.service");
+
 exports.recordSale = catchAsync(async (req, res, next) => {
-  const { _id: userId, company: companyId } = req.user;
+  const session = await mongoose.startSession();
+  const { _id: seller, company } = req.user;
 
-  // Extracting the products array, payment method, and customer name from the request body
-  const { products, paymentMethod, customerName } = req.body;
+  const {
+    items,
+    paymentMethod,
+    customer,
+    saleState,
+    packagingUnit,
+    totalPaid,
+  } = req.body;
 
-  // Looping over the products array and checking if the product exist in the company product DB
+  try {
+    const customerId = await resolveCustomerId(customer, company, session);
 
-  // Inside the product array loop, loop over the breakdown array and check if the requested amount is available.
+    const sale = await createSale(customerId, company, seller, session);
 
-  res.status(201).json({
-    message: "Processing...",
-  });
+    const saleId = sale[0]._id;
+
+    validateItems(items);
+
+    const companyProductMap = await getCompanyProductMap(items, session);
+
+    validateStock(items, companyProductMap);
+
+    const saleItems = await createSaleItems(items, saleId, session);
+
+    await deductStock(items, companyProductMap, session);
+
+    const totalSaleAmount = calculateTotalSaleAmount(saleItems);
+
+    const paymentStatus = getPaymentStatus(totalPaid, totalSaleAmount, next);
+
+    const updatedSale = await updateSale(
+      saleId,
+      totalSaleAmount,
+      totalPaid,
+      paymentStatus,
+      saleState.toUpperCase(),
+      session,
+    );
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        sale: updatedSale,
+        items: saleItems,
+      },
+    });
+  } catch (err) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error);
+  }
 });
